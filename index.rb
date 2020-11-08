20fbdd82b20e597b31817127c5bac03a4fa47445
EC2 Default User <ec2-user@ip-172-31-47-23.ap-northeast-1.compute.internal>
2020-11-08 12:19:00 +0000

    fizzbuzz

--- index.rb
+++ index.rb
@@ -1,1 +1,11 @@
-puts 'hello'
+
+def fizzbuzz(n)
+  puts "fizzbuzz" if n % 15 == 0
+  puts 'fizz' if n % 5 == 0
+  puts 'buzz' if n % 3 ==0
+  puts "#{n}" if n % 3 !=0 && n % 5 != 0
+end
+
+(1..33).each { |n|
+  print fizzbuzz(n)
+}
