2c861854bd547323ddc61cc806ab7a868e0c4020
EC2 Default User <ec2-user@ip-172-31-47-23.ap-northeast-1.compute.internal>
2020-11-08 14:04:52 +0000

    second

--- index.rb
+++ index.rb
@@ -1,11 +1,23 @@
 
-def fizzbuzz(n)
-  puts "fizzbuzz" if n % 15 == 0
-  puts 'fizz' if n % 5 == 0
-  puts 'buzz' if n % 3 ==0
-  puts "#{n}" if n % 3 !=0 && n % 5 != 0
+# def fizzbuzz(n)
+#   puts "fizzbuzz" if n % 15 == 0
+#   puts 'fizz' if n % 5 == 0
+#   puts 'buzz' if n % 3 ==0
+#   puts "#{n}" if n % 3 !=0 && n % 5 != 0
+# end
+
+# (1..33).each { |n|
+#   print fizzbuzz(n)
+# }
+
+
+def greeting(country)
+  return 'hey what' if country.nil?
+  if country == 'jap'
+    'こんにちは'
+  else 'america'
+  end
 end
 
-(1..33).each { |n|
-  print fizzbuzz(n)
-}
+p greeting('jap')
+p greeting('hw')
